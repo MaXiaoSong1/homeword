04ccce0bd184b439828492ed4149b8f4e19ebaf4
MaXiaoSong1 <1484896499@qq.com>
2022-07-21 19:44:26 +0800

    from封装

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,30 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/form",
+    name: "form",
+    component: () => import("../views/Form.vue"),
+  },
+  {
+    path: "/advert",
+    name: "advert",
+    component: () => import("../views/Advert.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
