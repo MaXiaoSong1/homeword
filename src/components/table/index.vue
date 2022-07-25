47e076371269fdadbafa0494d11e7d36e2afc784
MaXiaoSong1 <1484896499@qq.com>
2022-07-25 21:01:27 +0800

    上传

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -2,9 +2,9 @@
   <div>
     <el-table
       :data="tableData"
+      style="width: 100%"
       @selection-change="handleSelectionChange"
       @sort-change="handleSortChange"
-      style="width: 100%"
     >
       <el-table-column
         v-if="index"
@@ -17,42 +17,31 @@
         type="selection"
         width="55"
       ></el-table-column>
-      <template v-for="(item, index) in column">
-        <el-table-column
-          :render-header="item.renderHeader"
-          v-if="item.type === 'function'"
-          :key="index"
-          :prop="item.prop"
-          :label="item.label"
-          :width="item.width"
-        >
-          <template v-slot="scope">
-            <div
-              v-html="item.callback && item.callback(scope.row, index)"
-            ></div>
-          </template>
-        </el-table-column>
-        <el-table-column
-          v-if="item.type === 'slot'"
-          :render-header="item.renderHeader"
-          :key="index"
-          :prop="item.prop"
-          :label="item.label"
-          :width="item.width"
-        >
-          <template v-slot="scope">
-            <slot :name="item.slot_name" :data="scope.row"></slot>
-          </template>
-        </el-table-column>
-        <el-table-column
-          v-else
-          :render-header="item.renderHeader"
-          :key="index"
-          :prop="item.prop"
-          :label="item.label"
-          :width="item.width"
-        ></el-table-column>
-      </template>
+      <el-table-column
+        v-for="(item, index) in column"
+        :sort-by="item.sortBy"
+        :sortable="item.sort"
+        :render-header="item.renderHeader"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label"
+        :width="item.width"
+      >
+        <template v-slot="scope">
+          <slot
+            v-if="item.type === 'slot'"
+            :name="item.slot_name"
+            :data="scope.row"
+          ></slot>
+          <component
+            v-else
+            :data="scope.row"
+            :config="item"
+            :prop="item.prop"
+            :is="!item.type ? 'com-text' : `com-${item.type}`"
+          ></component>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -67,6 +56,10 @@ files.keys().forEach((item) => {
 });
 console.log(modules);
 export default {
+  name: "yangTable",
+  components: {
+    ...modules,
+  },
   props: {
     column: {
       type: Array,
@@ -110,7 +103,6 @@ export default {
   methods: {
     // 获取复选框选中的数据
     handleSelectionChange(val) {
-      console.log(val);
       this.$emit("update:checkList", val);
     },
     // 表格数据远程排序
@@ -118,39 +110,38 @@ export default {
       const sortBy = column.sortBy;
       this.$emit("sortTable", { sortBy, order });
     },
-    async getTableList() {
-      const url = this.url;
-      if (!url) {
-        throw new Error("url is required");
-        // return false;
-      }
-      try {
-        const requestData = {
-          url: this.url,
-          method: this.method,
-        };
+    // async getTableList() {
+    //   const url = this.url;
+    //   if (!url) {
+    //     throw new Error("url is required");
+    //     // return false;
+    //   }
+    //   try {
+    //     const requestData = {
+    //       url: this.url,
+    //       method: this.method,
+    //     };
 
-        if (JSON.stringify(this.data) === "{}") {
-          requestData.data = this.data;
-        }
+    //     if (JSON.stringify(this.data) === "{}") {
+    //       requestData.data = this.data;
+    //     }
 
-        if (JSON.stringify(this.params) === "{}") {
-          requestData.params = this.params;
-        }
-        const response = await this.$axios(requestData);
+    //     if (JSON.stringify(this.params) === "{}") {
+    //       requestData.params = this.params;
+    //     }
+    //     const response = await this.$axios(requestData);
 
-        let data = response.data.data;
+    //     let data = response.data.data;
+    //     if (this.format && typeof this.format === "function") {
+    //       data = this.format(response.data);
+    //     }
+    //     this.tableData = data;
 
-        if (this.format && typeof this.format === "function") {
-          data = this.format(response.data);
-        }
-        this.tableData = data;
-
-        this.onLoad && this.$emit("onLoad", response.data);
-      } catch (e) {
-        console.log(e);
-      }
-    },
+    //     this.onLoad && this.$emit("onLoad", response.data);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // },
     handleRequest() {
       this.getTableList();
     },
